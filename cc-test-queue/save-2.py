9864314f0d560da3f5b2c9b221ef58284b956aee
Linweicheng <linweicheng@iie.ac.cn>
2024-06-17 19:17:58 +0800

    u

--- cc-test-queue/save-2.py
+++ cc-test-queue/save-2.py
@@ -4,8 +4,10 @@ import struct
 # packet1 = b"SETUP rtsp://127.0.0.1:8554/wavAudioTest/track1 RTSP/1.0\r\nCSeq: 3\r\nUser-Agent: ./testRTSPClient (LIVE555 Streaming Media v2018.08.28)\r\nTransport: RTP/AVP;unicast;client_port=37952-37953\r\n\r\n"
 # packet2 = b"DESCRI:E rtsp://127.0.0.1:8\u001854/wav\r\n\r\n"
 
-packet1 = b"DESCRIBE rtsp://127.0.0.1:8554/wavAudioTest RTSP/1.0\r\nCSeq: 2\r\nUser-Agent: ./testRTSPClient (LIVE555 Streaming Media v2018.08.28)\r\nAccept: application/sdp\r\n\r\n"
-packet2 = b"DESCRIBE rtsp://127.0.0.1:8554/wavAudioTest RTSP/1.0\r\nCSe"
+# packet1 = b"DESCRIBE rtsp://127.0.0.1:8554/wavAudioTest RTSP/1.0\r\nCSeq: 2\r\nUser-Agent: ./testRTSPClient (LIVE555 Streaming Media v2018.08.28)\r\nAccept: application/sdp\r\n\r\n"
+# packet2 = b"DESCRIBE rtsp://127.0.0.1:8554/wavAudioTest RTSP/1.0\r\nCSe"
+
+packet1 = b"DESCRIFE rtsp://1pt: application"
 
 output_file_path = './cc-test-queue/packet_file'
 
@@ -14,6 +16,6 @@ with open(output_file_path, "wb") as f:
     # 写入第一个报文的大小和内容
     f.write(struct.pack("I", len(packet1)))
     f.write(packet1)
-    # 写入第二个报文的大小和内容
-    f.write(struct.pack("I", len(packet2)))
-    f.write(packet2)
+    # # 写入第二个报文的大小和内容
+    # f.write(struct.pack("I", len(packet2)))
+    # f.write(packet2)
