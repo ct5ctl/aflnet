2e897db502961219fead2155c1ed2841186fa0c8
Linweicheng <linweicheng@iie.ac.cn>
2024-06-17 19:13:00 +0800

    g

--- cc-test-queue/save-2.py
+++ cc-test-queue/save-2.py
@@ -1,8 +1,11 @@
 import struct
 
 # 准备报文内容
-packet1 = b"SETUP rtsp://127.0.0.1:8554/wavAudioTest/track1 RTSP/1.0\r\nCSeq: 3\r\nUser-Agent: ./testRTSPClient (LIVE555 Streaming Media v2018.08.28)\r\nTransport: RTP/AVP;unicast;client_port=37952-37953\r\n\r\n"
-packet2 = b"DESCRI:E rtsp://127.0.0.1:8\u001854/wav\r\n\r\n"
+# packet1 = b"SETUP rtsp://127.0.0.1:8554/wavAudioTest/track1 RTSP/1.0\r\nCSeq: 3\r\nUser-Agent: ./testRTSPClient (LIVE555 Streaming Media v2018.08.28)\r\nTransport: RTP/AVP;unicast;client_port=37952-37953\r\n\r\n"
+# packet2 = b"DESCRI:E rtsp://127.0.0.1:8\u001854/wav\r\n\r\n"
+
+packet1 = b"DESCRIBE rtsp://127.0.0.1:8554/wavAudioTest RTSP/1.0\r\nCSeq: 2\r\nUser-Agent: ./testRTSPClient (LIVE555 Streaming Media v2018.08.28)\r\nAccept: application/sdp\r\n\r\n"
+packet2 = b"DESCRIBE rtsp://127.0.0.1:8554/wavAudioTest RTSP/1.0\r\nCSe"
 
 output_file_path = './cc-test-queue/packet_file'
 
