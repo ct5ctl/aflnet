05b9155c79ade4b74a0e587cdeb7ba4ea6e33bb1
Linweicheng <linweicheng@iie.ac.cn>
2024-07-30 10:09:20 +0800

    1

--- afl-fuzz-global.h
+++ afl-fuzz-global.h
@@ -3,6 +3,53 @@
 #ifndef AFL_FUZZ_GLOBAL_H
 #define AFL_FUZZ_GLOBAL_H
 
+//afl-fuzz
+#define AFL_MAIN
+#include "android-ashmem.h"
+#define MESSAGES_TO_STDOUT
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#define _FILE_OFFSET_BITS 64
+
+#include "config.h"
+#include "types.h"
+#include "debug.h"
+#include "alloc-inl.h"
+#include "hash.h"
+#include "libs/cJSON.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <signal.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <dlfcn.h>
+#include <sched.h>
+
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/file.h>
+#include <sys/capability.h>
+
+#include "aflnet.h"
+#include <graphviz/gvc.h>
+#include <math.h>
+
+
 #include <sys/types.h>
 
 static u8 *stage_name = "init",       /* Name of the current fuzz stage   */
