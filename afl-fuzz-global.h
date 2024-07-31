3397635451301940890a66ca5c8dbc1da6dd357e
Linweicheng <linweicheng@iie.ac.cn>
2024-07-31 15:24:38 +0800

    1

--- afl-fuzz-global.h
+++ afl-fuzz-global.h
@@ -154,9 +154,9 @@ extern enum {
   /* 02 */ STAGE_VAL_BE
 };
 
-extern s8  interesting_8[];
-extern s16 interesting_16[];
-extern s32 interesting_32[];
+extern s8  interesting_8[1];
+extern s16 interesting_16[2];
+extern s32 interesting_32[3];
 // extern s8  interesting_8[]  = { INTERESTING_8 };
 // extern s16 interesting_16[] = { INTERESTING_8, INTERESTING_16 };
 // extern s32 interesting_32[] = { INTERESTING_8, INTERESTING_16, INTERESTING_32 };
